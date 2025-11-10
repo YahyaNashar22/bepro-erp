ec266b8f6e06f13ccd8c70b20fde82dd2bf997ae
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-11-10 06:45:58 +0200

    established socket connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const socket = io(import.meta.env.VITE_BACKEND_URL, {
+      transports: ["websocket"],
+    });
 
+    socket.on("connect", () => console.log("connected to server", socket.id));
+    socket.on("disconnect", () =>
+      console.log("disconnected from server", socket.id)
+    );
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Socket.IO Connected Frontend</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import "dotenv/config";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = process.env.PORT || 5000;
+
+app.get(
+    "/health", (req, res) => res.send("<h1>server running</h1>")
+);
+
+io.on(
+    "connection",
+    (socket) => {
+        // connection event
+        console.log("a user connected");
+        // disconnection event
+        socket.on("disconnect", () => console.log("a user disconnected"));
+    }
+);
+
+server.listen(port, () => {
+    console.log("listening on port", port);
+})
